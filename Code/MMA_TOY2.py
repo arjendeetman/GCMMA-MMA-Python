6d1bd340173e882342646e5d06c223880941872e
Arjen Deetman <info@arjendeetman.nl>
2019-10-13 20:22:41 +0200

    Changed folder structure

---
+++ Code/MMA_TOY2.py
@@ -0,0 +1,150 @@
+﻿########################################################################################################
+### MMA OPTIMIZATION ALGORITM															             ### 
+###                                                                                                  ###
+### Arjen Deetman                                                                                    ###
+### version 19-04-2017                                                                               ###
+########################################################################################################
+
+"""
+Orginal work Written by Krister Svanberg in Matlab. This is the python version of the code written
+by Arjen Deetman. 
+
+This script is the "toy problem":
+    minimize x(1)^2 + x(2)^2 + x(3)^2
+    subject to (x(1)-5)^2 + (x(2)-2)^2 + (x(3)-1)^2 =< 9
+               (x(1)-3)^2 + (x(2)-4)^2 + (x(3)-3)^2 =< 9
+               0 =< x(j) =< 5, for j=1,2,3.
+"""
+
+########################################################################################################
+### LOADING MODULES                                                                                  ###
+########################################################################################################
+
+# Loading modules
+from scipy.sparse import diags
+from scipy.linalg import solve
+import numpy as np
+import logging
+import sys
+
+# Import MMA functions
+from MMA import mmasub,subsolv,kktcheck
+
+
+########################################################################################################
+### MAIN FUNCTION                                                                                    ###
+########################################################################################################
+
+def main():
+    # Logger
+    logger=setup_logger("MMA_TOY2.log")
+    logger.info("Started\n")
+    # Set numpy print options
+    np.set_printoptions(precision=4, formatter={'float': '{: 0.4f}'.format})
+    # Beam initial settings
+    m=2
+    n=3
+    epsimin=0.0000001
+    eeen=np.ones((n,1))
+    eeem=np.ones((m,1))
+    zeron=np.zeros((n,1))
+    zerom=np.zeros((m,1))
+    xval=np.array([[4,3,2]]).T
+    xold1=xval.copy()
+    xold2=xval.copy()
+    xmin=zeron.copy()
+    xmax=5*eeen
+    low=xmin.copy()
+    upp=xmax.copy()
+    move=1.0
+    c=1000*eeem
+    d=eeem.copy()
+    a0=1
+    a=zerom.copy()
+    outeriter=0
+    maxoutit=11
+    kkttol=0		
+    # Calculate function values and gradients of the objective and constraints functions
+    if outeriter==0:
+        f0val,df0dx,fval,dfdx=toy2(xval)
+        innerit=0
+        outvector1=np.concatenate((np.array([outeriter]), xval.flatten()))
+        outvector2=np.concatenate((np.array([f0val]),fval.flatten()))
+        # Log
+        logger.info("outvector1 = {}".format(outvector1))
+        logger.info("outvector2 = {}\n".format(outvector2))
+    # The iterations starts
+    kktnorm=kkttol+10
+    outit=0
+    while (kktnorm>kkttol) and (outit<maxoutit):
+        outit+=1
+        outeriter+=1
+        # The MMA subproblem is solved at the point xval:
+        xmma,ymma,zmma,lam,xsi,eta,mu,zet,s,low,upp= \
+            mmasub(m,n,outeriter,xval,xmin,xmax,xold1,xold2,f0val,df0dx,fval,dfdx,low,upp,a0,a,c,d,move)
+        # Some vectors are updated:
+        xold2=xold1.copy()
+        xold1=xval.copy()
+        xval=xmma.copy()
+        # Re-calculate function values and gradients of the objective and constraints functions
+        f0val,df0dx,fval,dfdx=toy2(xval)
+        # The residual vector of the KKT conditions is calculated
+        residu,kktnorm,residumax= \
+            kktcheck(m,n,xmma,ymma,zmma,lam,xsi,eta,mu,zet,s,xmin,xmax,df0dx,fval,dfdx,a0,a,c,d)
+        outvector1=np.concatenate((np.array([outeriter]), xval.flatten()))
+        outvector2=np.concatenate((np.array([f0val]),fval.flatten()))
+        # Log
+        logger.info("outvector1 = {}".format(outvector1))
+        logger.info("outvector2 = {}".format(outvector2))
+        logger.info("kktnorm    = {}\n".format(kktnorm))
+    # Final log
+    logger.info("Finished")
+
+
+########################################################################################################
+### FUNCTIONS                                                                                        ###
+########################################################################################################
+
+# Setup logger
+def setup_logger(logfile):
+    # Create logger
+    logger=logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    # Create console handler and set level to debug
+    ch=logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    # Create file handler and set level to debug
+    fh=logging.FileHandler(logfile)
+    fh.setLevel(logging.DEBUG)
+    # Add formatter to ch and fh
+    formatter=logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    fh.setFormatter(formatter)
+    # Add ch and fh to logger
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+    # Open logfile and reset
+    with open(logfile, 'w'): pass
+    # Return logger
+    return logger
+
+# Toy function
+def toy2(xval):
+    f0val=xval[0][0]**2+xval[1][0]**2+xval[2][0]**2
+    df0dx=2*xval
+    fval1=((xval.T-np.array([[5, 2, 1]]))**2).sum()-9
+    fval2=((xval.T-np.array([[3, 4, 3]]))**2).sum()-9
+    fval=np.array([[fval1,fval2]]).T
+    dfdx1=2*(xval.T-np.array([[5, 2, 1]]))
+    dfdx2=2*(xval.T-np.array([[3, 4, 3]]))
+    dfdx=np.concatenate((dfdx1,dfdx2))
+    return f0val,df0dx,fval,dfdx
+
+
+########################################################################################################
+### RUN MAIN FUNCTION                                                                                ###
+########################################################################################################
+
+# Run main function / program
+if __name__ == "__main__":
+    main()
