3c77106fd2aa6dfc54414f31a7b2e442cfbb4a4c
arjendeetman <arjendeetman@hotmail.com>
2024-08-05 21:50:55 +0200

    Update __init__.py

---
+++ scr/gcmma-mma-py/__init__.py
@@ -0,0 +1,7 @@
+"""
+GCMMA-MMA-Python
+
+This file is part of GCMMA-MMA-Python. GCMMA-MMA-Python is licensed under the terms of GNU 
+General Public License as published by the Free Software Foundation. For more information and 
+the LICENSE file, see <https://github.com/arjendeetman/GCMMA-MMA-Python>. 
+"""
