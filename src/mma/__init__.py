a1ca19f14ca7964c179eebe23a80e3f73f34729c
arjendeetman <arjendeetman@hotmail.com>
2024-08-06 12:40:09 +0200

    Update file structure

--- src/mma/__init__.py
+++ src/mma/__init__.py
@@ -5,3 +5,5 @@ This file is part of GCMMA-MMA-Python. GCMMA-MMA-Python is licensed under the te
 General Public License as published by the Free Software Foundation. For more information and 
 the LICENSE file, see <https://github.com/arjendeetman/GCMMA-MMA-Python>. 
 """
+
+from .mma import mmasub, gcmmasub, subsolv, kktcheck, raaupdate, concheck, asymp
